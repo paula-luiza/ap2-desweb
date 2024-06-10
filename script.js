eff53b08f6ebb69413a5a871b48980bf18e55580
paula-luiza <paulalro8@gmail.com>
2024-06-10 06:07:31 -0300

    update

--- script.js
+++ script.js
@@ -12,30 +12,91 @@ divPesquisa.appendChild(inputPesquisa);
 
 document.body.appendChild(divPesquisa);
 
-const conteudo = document.createElement('div');
 
+const conteudo = document.createElement('div');
+conteudo.style.display = 'flex';
+conteudo.style.flexWrap = 'wrap';
+conteudo.style.justifyContent = 'center';
+conteudo.style.alignItems = 'center';
+conteudo.style.gap = '10px';
+conteudo.innerHTML = 'carregando...';
 
 document.body.appendChild(conteudo);
 
 const montaCard = (entrada) => {
     const card = document.createElement('div');
+    card.style.display = 'grid';
+    card.style.gridTemplateColumns = '1fr 2fr';
+    card.style.gridTemplateAreas = `
+    "a1 a2"
+    "a1 a3"
+    "a4 a4"
+    "a5 a5"
+    `;
+    card.style.width = '30rem';
+    card.style.border = 'solid';
+    card.style.padding = '.3rem';
+
+    card.dataset.id = entrada.id;
+    card.dataset.elenco = entrada.elenco;
+    card.dataset.nome = entrada.nome;
+    card.dataset.posicao = entrada.posicao;
+    card.dataset.imagem = entrada.imagem;
+    card.dataset.detalhes = entrada.detalhes;
+    card.dataset.nomeCompleto = entrada.nome_completo;
+    card.dataset.nascimento = entrada.nascimento;
+    card.dataset.altura = entrada.altura;
+    card.dataset.n_jogos = entrada.n_jogos;
+    card.dataset.naturalidade = entrada.naturalidade;
+    card.dataset.no_botafogo_desde = entrada.no_botafogo_desde;
+
 
     const imgContainer = document.createElement('div');
-    
+    imgContainer.style.gridArea = 'a1';
+    imgContainer.style.display = 'flex';
+    imgContainer.style.alignItems = 'center';
+    imgContainer.style.justifyContent = 'center';
+
     const imagem = document.createElement('img');
     imagem.src = entrada.imagem;
+    imagem.alt = `Foto de ${entrada.nome}`;
+    imagem.style.width = '7rem';
+    imagem.style.height = '7rem';
+    imagem.style.borderRadius = '50%';
+    imagem.style.objectFit = 'cover';
+    imagem.style.objectPosition = 'top';
 
     const posicao = document.createElement('p');
     posicao.innerHTML = entrada.posicao;
+    posicao.style.cssText = `
+        grid-area: a2;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-transform: uppercase;
+    `;
+    /*posicao.style.gridArea = 'a2';
+    posicao.style.display = 'flex';
+    posicao.style.alignItems = 'center';
+    posicao.style.justifyContent = 'center';
+    posicao.style.textTransform=  'uppercase';*/
 
     const nome = document.createElement('p');
     nome.innerHTML = entrada.nome;
+    /*nome.className = 'nome';*/
+    nome.style.gridArea = 'a3';
+    nome.style.display = 'flex';
+    nome.style.alignItems = 'center';
+    nome.style.justifyContent = 'center';
+    nome.style.fontWeight = 'bold';
 
     const detalhes = document.createElement('p');
     detalhes.innerHTML = entrada.detalhes;
-    
+    detalhes.style.gridArea = 'a4';
+
     const nascimento = document.createElement('p');
     nascimento.innerHTML = entrada.nascimento;
+    nascimento.style.gridArea = 'a5';
 
     card.appendChild(imgContainer);
     imgContainer.appendChild(imagem);
@@ -44,7 +105,53 @@ const montaCard = (entrada) => {
     card.appendChild(detalhes);
     card.appendChild(nascimento);
 
+    card.onclick = handleClick;
+
     return card;
+}
+
+
+
+inputPesquisa.onkeyup = (ev) => {
+    console.log(ev.target.value);
+    
+    if (ev.target.value.length > 3 || ev.target.value.length == 0){
+        const filtrado = dados.filter(
+            (elemento) => {
+                const estaNoNome = elemento.nome.toLowerCase().includes(ev.target.value.toLowerCase());
+                const estaNaPosicao = elemento.posicao.toLowerCase().includes(ev.target.value.toLowerCase());
+                return estaNoNome || estaNaPosicao;
+            }
+        )
+    
+        conteudo.innerHTML = '';
+    
+        filtrado.forEach(
+            (atleta) => {
+                conteudo.appendChild(montaCard(atleta));
+            }
+        )
+    }
+
+    
+}
+
+const handleClick = ( evento ) => {
+    const card = evento.target.closest('div'); 
+    for (const propriedade in card.dataset){
+        
+        //cookies
+        document.cookie = `${propriedade}=${card.dataset[propriedade]}`;
+
+        //localStorage item por item
+        //localStorage.setItem(propriedade, card.dataset[propriedade]);
+
+    }
+
+
+    localStorage.setItem('atleta', JSON.stringify(card.dataset))
+
+    window.location.href = `detalhes.html?id=${card.dataset.id}`;
 }
 
 
@@ -54,13 +161,15 @@ const pegaDados = async (caminho) => {
     return dados;
 }
 
-
 pegaDados("https://botafogo-atletas.mange.li/2024-1/feminino").then(
     (entrada) => {
         dados = entrada;
+        conteudo.innerHTML = '';
         dados.forEach(
             (atleta) => {
                 conteudo.appendChild(montaCard(atleta));
             }
         )
     });
+
+console.log('síncrono')
